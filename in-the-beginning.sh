6df65b6ea3db0477d14ae7d3c67e189acf5cffc5
iyu <iyu@vlapwd.com>
2025-03-04 17:29:40 +0900

    add

---
+++ in-the-beginning.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# vimrc
+if [ ! -L ~/.vimrc ]; then
+ln -s $(pwd)/.vimrc ~/.vimrc
+fi
+
+# homebrew
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if [[ "$(uname -m)" == "arm64" ]]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.zshrc
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
+
+  brew -v
+else
+  echo "Homebrewは既にインストールされています。$(brew -v)"
+fi
+
+# nvm
+if ! command -v nvm &>/dev/null; then
+  # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+else
+  echo "nvm aru $(nvm -v)"
+fi
